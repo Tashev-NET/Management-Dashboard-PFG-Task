45df53cd81e98a9b434c046b6985cb617ff6cb62
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-03-29 14:09:58 +0200

    Added Comment Interface

--- src/Types.ts
+++ src/Types.ts
@@ -1,16 +1,24 @@
 export interface User {
-    id: number;
-    name: string;
-    username: string;
-    email: string;
-  }
-  export interface Post {
-    id: number;
-    title: string;
-    body: string;
-    userId: number;
-  }
-  export interface Album {
-    id: number;
-    title: string;
-  }
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
+export interface Post {
+  id: number;
+  title: string;
+  body: string;
+  userId: number;
+}
+export interface Album {
+  id: number;
+  title: string;
+}
+
+export interface Comment {
+  id: number;
+  postId: number;
+  body: string;
+  email: string;
+  name: string;
+}
