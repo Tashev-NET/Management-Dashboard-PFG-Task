55495c28299c1d716e3e653d334d3625e89ca0f5
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-03-29 14:23:45 +0200

    Added Simulate Delete Functionality For Post

--- src/screens/PostComments.tsx
+++ src/screens/PostComments.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { fetchPostComments } from "../api";
 import { Comment } from "../Types";
 import CommentCard from "../components/CommentCard";
@@ -8,13 +8,24 @@ const PostComments: React.FC = () => {
   const [comments, setComments] = useState<Comment[]>([]);
   const { id } = useParams<{ id: string }>();
 
+  const navigate = useNavigate();
   useEffect(() => {
     fetchPostComments(id ?? "").then((data) => setComments(data));
   }, [id]);
 
   return (
     <div className="container mx-auto">
-      <h1 className="text-2xl font-bold my-4">Post Comments</h1>
+      <h1 className="text-2xl font-bold my-4 flex justify-between  items-center">
+        Post Comments
+        <button
+          onClick={() => {
+            navigate(-1);
+          }}
+          className="text-red-600 hover:underline"
+        >
+          Delete Post
+        </button>
+      </h1>
       <div className="grid grid-cols-2 gap-4">
         {comments.map((comment) => (
           <CommentCard key={comment.id} comment={comment} />
