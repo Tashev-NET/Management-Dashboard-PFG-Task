0c36fbba5bc8c8331805d06d3c0ea51c8b7d9156
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-03-29 12:38:24 +0200

    Added UserAlbums & UserPosts Screens

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import Albums from "./screens/Albums";
 import Posts from "./screens/Posts";
 import Home from "./screens/Home";
 import Sidebar from "./components/Sidebar";
+import UserAlbums from "./screens/UserAlbums";
+import UserPosts from "./screens/UserPosts";
 
 const App = () => {
   return (
@@ -11,8 +13,11 @@ const App = () => {
       <div className=" flex flex-row w-full mt-10">
         <Sidebar />
         <Routes>
+          <Route path="/user/:id/albums" element={<UserAlbums />} />
+          <Route path="/user/:id/posts" element={<UserPosts />} />
           <Route path="/" element={<Home />} />
           <Route path="/users" element={<Users />} />
+
           <Route path="/albums" element={<Albums />} />
           <Route path="/posts" element={<Posts />} />
         </Routes>
--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { User } from "../Types";
+import { Link } from "react-router-dom";
 
 type UserCardProps = {
   user: User;
@@ -11,6 +12,14 @@ const UserCard: React.FC<UserCardProps> = ({ user }) => {
       <h2 className="text-xl font-semibold">{user.name}</h2>
       <p className="text-gray-600">@{user.username}</p>
       <p className="text-gray-600">{user.email}</p>
+      <div className="mt-4">
+        <Link to={`/user/${user.id}/albums`} className="mr-4 text-blue-600">
+          Albums
+        </Link>
+        <Link to={`/user/${user.id}/posts`} className="text-blue-600">
+          Posts
+        </Link>
+      </div>
     </div>
   );
 };
