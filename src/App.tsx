4d8f77dab96ce2d936e81ac02d0fd72b2d6ed7b7
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-03-29 12:46:22 +0200

    Fix Error With: 'Link' is declared but its value is never read.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Users from "./screens/Users";
 import Albums from "./screens/Albums";
 import Posts from "./screens/Posts";
