0ef654401d34c99ffa5b457126fb1a7488694f63
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-03-29 18:39:23 +0200

    Added "Author username" In AlbumCard Component

--- src/components/AlbumCard.tsx
+++ src/components/AlbumCard.tsx
@@ -1,16 +1,21 @@
-// components/AlbumCard.tsx
-
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Album } from "../Types";
+import { fetchPostUserData } from "../api";
 
 interface AlbumCardProps {
   album: Album;
 }
 
 const AlbumCard: React.FC<AlbumCardProps> = ({ album }) => {
+  const [userData, setUserData] = useState<any>();
+
+  useEffect(() => {
+    fetchPostUserData(album.userId).then((data) => setUserData(data));
+  }, []);
   return (
     <div className="bg-white shadow-md p-4 rounded-md">
       <h2 className="text-xl font-semibold">{album.title}</h2>
+      <p className="text-gray-600">Author username: {userData?.username}</p>
     </div>
   );
 };
