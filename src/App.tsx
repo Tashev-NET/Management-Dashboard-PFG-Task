f14e7ce69cd047373e7eb17191c5f5329be4f0c9
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-03-29 10:25:09 +0200

    Install "Tailwind CSS"

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div>App</div>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      Management Dashboard - PFG Task
+    </h1>
+  );
 };
 
 export default App;
