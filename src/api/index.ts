cc3cf34e9db0574de3d898b8c8e0f48568a6a7c6
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-03-29 11:52:08 +0200

    Added fetchPosts Functionality

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,22 @@
-// api.ts
-
 export const fetchUsers = async () => {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     if (!response.ok) {
       throw new Error("Failed to fetch users");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+
+export const fetchPosts = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+    if (!response.ok) {
+      throw new Error("Failed to fetch posts");
     }
     const data = await response.json();
     return data;
