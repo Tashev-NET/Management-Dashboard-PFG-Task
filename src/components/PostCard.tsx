61ca62e5f21bd3ef48b68a5640eabccbe59e8384
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-03-29 14:42:20 +0200

    Added Author email In PostCard Component

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Post } from "../Types";
+import { fetchPostUserData } from "../api";
 
 type PostCardProps = {
   post: Post;
 };
 
 const PostCard: React.FC<PostCardProps> = ({ post }) => {
+  const [userData, setUserData] = useState<any>();
+
+  useEffect(() => {
+    fetchPostUserData(post.userId).then((data) => setUserData(data));
+  }, []);
+
   return (
     <div className="bg-white shadow-md p-4 rounded-md mb-3">
       <h2 className="text-xl font-semibold">{post.title}</h2>
       <p className="text-gray-600">{post.body}</p>
-      <p className="text-gray-600">User ID: {post.userId}</p>
+      <p className="text-gray-600">Author email:{userData?.email}</p>
     </div>
   );
 };
