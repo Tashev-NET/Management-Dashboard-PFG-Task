ca7b7dc371f673ec8815706a475b3525d95ad5b6
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-03-29 11:09:45 +0200

    Added 'cursor-pointer' Styling For Every Element In Sidebar Component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,9 +5,30 @@ const Sidebar = () => {
   return (
     <div className="w-[20%]">
       <ul className="flex flex-col items-center gap-15">
-        <li onClick={()=>{navigate('/users')}}>users</li>
-        <li onClick={()=>{navigate('/albums')}}>albums</li>
-        <li onClick={()=>{navigate('/posts')}}>posts</li>
+        <li
+          className="cursor-pointer"
+          onClick={() => {
+            navigate("/users");
+          }}
+        >
+          users
+        </li>
+        <li
+          className="cursor-pointer"
+          onClick={() => {
+            navigate("/albums");
+          }}
+        >
+          albums
+        </li>
+        <li
+          className="cursor-pointer"
+          onClick={() => {
+            navigate("/posts");
+          }}
+        >
+          posts
+        </li>
       </ul>
     </div>
   );
