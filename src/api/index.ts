3e54c7b3913993bd6e9794ee1ad0fd93b1c90ff0
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-03-29 11:30:51 +0200

    Added fetchUsers Functionality

---
+++ src/api/index.ts
@@ -0,0 +1,15 @@
+// api.ts
+
+export const fetchUsers = async () => {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    if (!response.ok) {
+      throw new Error("Failed to fetch users");
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
