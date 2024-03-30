291dc0a4c33dbbdbaa9781b393046584556ec176
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-03-30 17:21:19 +0200

    Added "Favorite Album" Functionality

--- src/components/AlbumCard.tsx
+++ src/components/AlbumCard.tsx
@@ -1,20 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { Album } from "../Types";
 import { fetchPostUserData } from "../api";
-
+import { Star } from "lucide-react";
 interface AlbumCardProps {
   album: Album;
+  isFavorite: boolean;
+  setFavoriteAlbums: React.Dispatch<React.SetStateAction<number[]>>;
 }
 
-const AlbumCard: React.FC<AlbumCardProps> = ({ album }) => {
+const AlbumCard: React.FC<AlbumCardProps> = ({
+  album,
+  isFavorite,
+  setFavoriteAlbums,
+}) => {
   const [userData, setUserData] = useState<any>();
-
   useEffect(() => {
     fetchPostUserData(album.userId).then((data) => setUserData(data));
   }, []);
+
+  const favoriteAlbum = () => {
+    setFavoriteAlbums((prev) => [...prev, album.id]);
+  };
+  const unFavoriteAlbum = () => {
+    setFavoriteAlbums((prev) => prev.filter((el) => el !== album.id));
+  };
   return (
     <div className="bg-white shadow-md p-4 rounded-md">
-      <h2 className="text-xl font-semibold">{album.title}</h2>
+      <div className="flex justify-between">
+        <h2 className="text-xl font-semibold">{album.title}</h2>
+        {isFavorite ? (
+          <Star fill="yellow" onClick={favoriteAlbum} />
+        ) : (
+          <Star color="black" onClick={unFavoriteAlbum} />
+        )}
+      </div>
       <p className="text-gray-600">Author username: {userData?.username}</p>
     </div>
   );
--- src/screens/Albums.tsx
+++ src/screens/Albums.tsx
@@ -2,10 +2,14 @@ import React, { useState, useEffect } from "react";
 import { fetchAlbums } from "../api";
 import AlbumCard from "../components/AlbumCard";
 import { Album } from "../Types";
+import { useLocalStorage } from "../hooks/useLocalStorage";
 
 const Albums: React.FC = () => {
   const [albums, setAlbums] = useState<Album[]>([]);
-
+  const [favoriteAlbums, setFavoriteAlbums] = useLocalStorage<number[]>(
+    "favoriteAlbums",
+    []
+  );
   useEffect(() => {
     fetchAlbums().then((data) => setAlbums(data));
   }, []);
@@ -15,7 +19,12 @@ const Albums: React.FC = () => {
       <h1 className="text-2xl font-bold my-4">Albums</h1>
       <div className="grid grid-cols-2 gap-4">
         {albums.map((album) => (
-          <AlbumCard key={album.id} album={album} />
+          <AlbumCard
+            key={album.id}
+            album={album}
+            setFavoriteAlbums={setFavoriteAlbums}
+            isFavorite={favoriteAlbums.includes(album.id)}
+          />
         ))}
       </div>
     </div>
