e20ba3ce97ca6bc702b2138c2cbbc8178c4d4f63
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-04-01 15:54:29 +0300

    Make Position Sticky For Sidebar Component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -8,63 +8,65 @@ const Sidebar = () => {
   const navigate = useNavigate();
 
   return (
-    <div className="w-1/5 pr-4">
-      <div className="hidden md:flex justify-center">
-        <ul className="flex flex-col gap-4">
-          <SidebarItem
-            icon={<Users />}
-            text="users"
-            path="/users"
-            navigate={navigate}
-          />
-          <SidebarItem
-            icon={<Library />}
-            text="albums"
-            path="/albums"
-            navigate={navigate}
-          />
-          <SidebarItem
-            icon={<Rss />}
-            text="posts"
-            path="/posts"
-            navigate={navigate}
-          />
-        </ul>
-      </div>
+    <div className="w-1/5 pr-4 relative">
+      <div className=" sticky top-4 left-0">
+        <div className="hidden md:flex justify-center">
+          <ul className="flex flex-col gap-4">
+            <SidebarItem
+              icon={<Users />}
+              text="users"
+              path="/users"
+              navigate={navigate}
+            />
+            <SidebarItem
+              icon={<Library />}
+              text="albums"
+              path="/albums"
+              navigate={navigate}
+            />
+            <SidebarItem
+              icon={<Rss />}
+              text="posts"
+              path="/posts"
+              navigate={navigate}
+            />
+          </ul>
+        </div>
 
-      <div className="md:hidden">
-        <div
-          onClick={() => {
-            setIsOpen((prev) => !prev);
-          }}
-          className="flex justify-end cursor-pointer"
-        >
-          {isOpen ? <X /> : <Menu />}
-        </div>{" "}
-        {isOpen && (
-          <div className="flex justify-center">
-            <ul className="flex flex-col gap-4">
-              <SidebarItem
-                icon={<Users />}
-                text="users"
-                path="/users"
-                navigate={navigate}
-              />
-              <SidebarItem
-                icon={<Library />}
-                text="albums"
-                path="/albums"
-                navigate={navigate}
-              />
-              <SidebarItem
-                icon={<Rss />}
-                text="posts"
-                path="/posts"
-                navigate={navigate}
-              />
-            </ul>
-          </div>
-        )}
+        <div className="md:hidden">
+          <div
+            onClick={() => {
+              setIsOpen((prev) => !prev);
+            }}
+            className="flex justify-end cursor-pointer"
+          >
+            {isOpen ? <X /> : <Menu />}
+          </div>{" "}
+          {isOpen && (
+            <div className="flex justify-center">
+              <ul className="flex flex-col gap-4">
+                <SidebarItem
+                  icon={<Users />}
+                  text="users"
+                  path="/users"
+                  navigate={navigate}
+                />
+                <SidebarItem
+                  icon={<Library />}
+                  text="albums"
+                  path="/albums"
+                  navigate={navigate}
+                />
+                <SidebarItem
+                  icon={<Rss />}
+                  text="posts"
+                  path="/posts"
+                  navigate={navigate}
+                />
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
