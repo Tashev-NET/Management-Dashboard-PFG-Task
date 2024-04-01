bf91896811fcf7a85749a083973b60dd9378ad8f
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-04-01 11:31:36 +0300

    Added Height For PostCard Component

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -7,7 +7,7 @@ type PostCardProps = {
 
 const PostCard: React.FC<PostCardProps> = ({ post }) => {
   return (
-    <div className="bg-white shadow-md p-4 rounded-md mb-3">
+    <div className="bg-white shadow-md p-4 rounded-md mb-3 h-[170px]">
       <h2 className="text-xl font-semibold">{post.title}</h2>
       <p className="text-gray-600">{post.body}</p>
       {/* <p className="text-gray-600">Author email:{userData?.email}</p> */}
