7a12760d21011804f26bfa09c25154f0e988556c
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-04-01 14:33:02 +0300

    Added SideBar Mobile Version Logic

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,35 +1,79 @@
+import { useState } from "react";
 import { useNavigate } from "react-router";
-
+import { Menu } from "lucide-react";
 const Sidebar = () => {
+  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
   return (
-    <div className="w-[20%]">
-      <ul className="flex flex-col items-center gap-15">
-        <li
-          className="cursor-pointer"
-          onClick={() => {
-            navigate("/users");
-          }}
-        >
-          users
-        </li>
-        <li
-          className="cursor-pointer"
-          onClick={() => {
-            navigate("/albums");
-          }}
-        >
-          albums
-        </li>
-        <li
-          className="cursor-pointer"
-          onClick={() => {
-            navigate("/posts");
-          }}
-        >
-          posts
-        </li>
-      </ul>
+    <div className="w-[20%] pr-4">
+      {window.innerWidth >= 768 && (
+        <ul className="flex flex-col items-center gap-15">
+          <li
+            className="cursor-pointer"
+            onClick={() => {
+              navigate("/users");
+            }}
+          >
+            users
+          </li>
+          <li
+            className="cursor-pointer"
+            onClick={() => {
+              navigate("/albums");
+            }}
+          >
+            albums
+          </li>
+          <li
+            className="cursor-pointer"
+            onClick={() => {
+              navigate("/posts");
+            }}
+          >
+            posts
+          </li>
+        </ul>
+      )}
+      {window.innerWidth <= 768 && (
+        <div>
+          <div
+            onClick={() => {
+              setIsOpen((prev) => !prev);
+            }}
+            className="flex justify-end"
+          >
+            <Menu />
+          </div>{" "}
+          {isOpen && (
+            <ul className="flex flex-col items-center gap-15">
+              <li
+                className="cursor-pointer"
+                onClick={() => {
+                  navigate("/users");
+                }}
+              >
+                users
+              </li>
+              <li
+                className="cursor-pointer"
+                onClick={() => {
+                  navigate("/albums");
+                }}
+              >
+                albums
+              </li>
+              <li
+                className="cursor-pointer"
+                onClick={() => {
+                  navigate("/posts");
+                }}
+              >
+                posts
+              </li>
+            </ul>
+          )}
+        </div>
+      )}
     </div>
   );
 };
