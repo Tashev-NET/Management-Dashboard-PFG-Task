08f487b462e594fa9d7a6c3535bed76cd13220ca
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-04-01 14:55:14 +0300

    Fix innerWidth Value & Added Conditional Rendering For Menu & Close Icons In Sidebar Component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,12 +1,12 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
-import { Album, Library, Menu, Rss, Users } from "lucide-react";
+import {  Library, Menu, Rss, Users, X } from "lucide-react";
 const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
   return (
     <div className="w-[20%] pr-4">
-      {window.innerWidth >= 768 && (
+      {window.innerWidth >= 800 && (
         <div className="flex justify-center">
           <ul className="flex flex-col gap-[15px] ">
             <li
@@ -38,47 +38,47 @@ const Sidebar = () => {
           </ul>
         </div>
       )}
-      {window.innerWidth <= 768 && (
+      {window.innerWidth <= 800 && (
         <div>
           <div
             onClick={() => {
               setIsOpen((prev) => !prev);
             }}
-            className="flex justify-end"
+            className="flex justify-end cursor-pointer"
           >
-            <Menu />
+            {isOpen ? <X /> : <Menu />}
           </div>{" "}
           {isOpen && (
             <div className="flex justify-center">
-            <ul className="flex flex-col gap-[15px] ">
-              <li
-                className="cursor-pointer flex gap-4"
-                onClick={() => {
-                  navigate("/users");
-                }}
-              >
-                <Users /> <p>users</p>
-              </li>
-              <li
-                className="cursor-pointer flex gap-4"
-                onClick={() => {
-                  navigate("/albums");
-                }}
-              >
-                <Library />
-                <p>albums</p>
-              </li>
-              <li
-                className="cursor-pointer flex gap-4"
-                onClick={() => {
-                  navigate("/posts");
-                }}
-              >
-                <Rss />
-                <p>posts</p>
-              </li>
-            </ul>
-          </div>
+              <ul className="flex flex-col gap-[15px] ">
+                <li
+                  className="cursor-pointer flex gap-4"
+                  onClick={() => {
+                    navigate("/users");
+                  }}
+                >
+                  <Users /> <p>users</p>
+                </li>
+                <li
+                  className="cursor-pointer flex gap-4"
+                  onClick={() => {
+                    navigate("/albums");
+                  }}
+                >
+                  <Library />
+                  <p>albums</p>
+                </li>
+                <li
+                  className="cursor-pointer flex gap-4"
+                  onClick={() => {
+                    navigate("/posts");
+                  }}
+                >
+                  <Rss />
+                  <p>posts</p>
+                </li>
+              </ul>
+            </div>
           )}
         </div>
       )}
