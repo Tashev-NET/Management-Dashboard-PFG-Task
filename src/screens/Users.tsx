ec15d5c5cfee71d6b03e4619f7550154ee328744
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-04-01 16:42:20 +0300

    Remove margin:auto; From Users Screen

--- src/screens/Users.tsx
+++ src/screens/Users.tsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Users: React.FC = () => {
   const { users } = useAppContext();
   return (
-    <div className="container mx-auto px-4">
+    <div className="container px-4">
       <h1 className="text-2xl font-bold my-4">Users</h1>
       <div className="grid grid-cols-2 gap-4">
         {users.map((user) => (
