8320f22948626343ebc1347b1f343cb8da3358bc
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-04-04 16:14:18 +0300

    Added renderMenu Function In Sidebar Component

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -7,31 +7,35 @@ const Sidebar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
 
+  const renderMenu = () => (
+    <ul className="flex flex-col gap-4">
+      <SidebarItem
+        icon={<Users />}
+        text="users"
+        path="/users"
+        navigate={navigate}
+      />
+
+      <SidebarItem
+        icon={<Library />}
+        text="albums"
+        path="/albums"
+        navigate={navigate}
+      />
+
+      <SidebarItem
+        icon={<Rss />}
+        text="posts"
+        path="/posts"
+        navigate={navigate}
+      />
+    </ul>
+  );
+
   return (
     <div className="w-1/5 pr-4 relative">
       <div className=" sticky top-4 left-0">
-        <div className="hidden md:flex justify-center">
-          <ul className="flex flex-col gap-4">
-            <SidebarItem
-              icon={<Users />}
-              text="users"
-              path="/users"
-              navigate={navigate}
-            />
-            <SidebarItem
-              icon={<Library />}
-              text="albums"
-              path="/albums"
-              navigate={navigate}
-            />
-            <SidebarItem
-              icon={<Rss />}
-              text="posts"
-              path="/posts"
-              navigate={navigate}
-            />
-          </ul>
-        </div>
+        <div className="hidden md:flex justify-center">{renderMenu()}</div>
 
         <div className="md:hidden">
           <div
@@ -41,31 +45,8 @@ const Sidebar = () => {
             className="flex justify-end cursor-pointer mb-4"
           >
             {isOpen ? <X /> : <Menu />}
-          </div>{" "}
-          {isOpen && (
-            <div className="flex justify-center">
-              <ul className="flex flex-col gap-4">
-                <SidebarItem
-                  icon={<Users />}
-                  text="users"
-                  path="/users"
-                  navigate={navigate}
-                />
-                <SidebarItem
-                  icon={<Library />}
-                  text="albums"
-                  path="/albums"
-                  navigate={navigate}
-                />
-                <SidebarItem
-                  icon={<Rss />}
-                  text="posts"
-                  path="/posts"
-                  navigate={navigate}
-                />
-              </ul>
-            </div>
-          )}
+          </div>
+          {isOpen && <div className="flex justify-center">{renderMenu()}</div>}
         </div>
       </div>
     </div>
